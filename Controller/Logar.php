0dfe99d9e6211eeba0d3fdff67899e8106af2570
Diogo Alves <45717126+diogoasp@users.noreply.github.com>
2022-11-23 16:43:01 -0300

    Correção do Login

--- Controller/Logar.php
+++ Controller/Logar.php
@@ -3,6 +3,9 @@
 
     if(confereCadastro($_POST['username'], $_POST['password'])){
         setcookie("autenticacao", $_POST['username'], time()+36000, "/"); 
+        session_start();
+        $_SESSION['username'] = $_POST['username'];
+        $_SESSION['password'] = $_POST['password'];
         header('Location:'.'../perfil.php');
     }else{ 
         header('Location:'.'../index.php?erro=1');
--- resources/header.php
+++ resources/header.php
@@ -1,18 +1,18 @@
 <ul id="menu" class="flex horizontal center">
-<li>
-    <img id="logo" src="imagens/android-chrome-192x192.png" alt="sociallife logo">
-</li>
-<li>
-    <a href="perfil.php"> <i class="bi bi-person-circle"></i>Perfil</a>
-</li>
-<li>
-    <a href="feed.php"> <i class="bi bi-house-fill"></i> Feed</a>
-</li>
-<li>
-    <a href="usuarios.php"> <i class="bi bi-person-lines-fill"></i> Usuários</a>
-</li>
-<li>
-    <a href="resources/sair.php"><i class="bi bi-door-open"></i>Sair</a>
-</li>
+    <li>
+        <img id="logo" src="imagens/android-chrome-192x192.png" alt="sociallife logo">
+    </li>
+    <li>
+        <a href="perfil.php"> <i class="bi bi-person-circle"></i>Perfil</a>
+    </li>
+    <li>
+        <a href="feed.php"> <i class="bi bi-house-fill"></i> Feed</a>
+    </li>
+    <li>
+        <a href="usuarios.php"> <i class="bi bi-person-lines-fill"></i> Usuários</a>
+    </li>
+    <li>
+        <a href="resources/sair.php"><i class="bi bi-door-open"></i>Sair</a>
+    </li>
     
 </ul>
